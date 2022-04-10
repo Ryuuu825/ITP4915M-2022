7bb98733f48e5d77ec20795c67432a9c5be21a6d
Ryuuu825 <Ryuuu825@protonmail.com>
2022-04-10 23:54:05 +0800

    Created Render
    not yet finished

---
+++ TheBetterLimited-ManagementSystem/Lib/Toolkit/Control/Testing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace TheBetterLimited_ManagementSystem.Lib.Toolkit.Control
+{
+    public class Testing : System.Windows.Forms.Control
+    {
+        
+        private Rectangle rect;
+        
+
+        public Testing()
+        {
+            rect = new Rectangle(0,0,10,10);
+            this.Paint += Init_Paint;
+            this.Text = "Texting";
+
+        }
+
+        protected void Init_Paint(object sender, PaintEventArgs e)
+        {
+            Render.instance.DrawRect(this.rect, e.Graphics, Colour.StandardColour.SecondaryColour).FillRect(this.rect, e.Graphics, Colour.StandardColour.MainColour);
+            Render.instance.DrawString(this.Text, e.Graphics, 18, Color.White, this.rect);
+            Render.instance.DrawCircle( e.Graphics, Color.Red , 10, 10, 5);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            rect = new Rectangle(0,0,this.Width,this.Height);
+            this.Refresh();
+        }
+    }
+}
--- TheBetterLimited-ManagementSystem/Test/TestText.Designer.cs
+++ TheBetterLimited-ManagementSystem/Test/TestText.Designer.cs
@@ -28,52 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.headerTwo1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderTwo();
-            this.headerThree1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderThree();
-            this.headerFour1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderFour();
-            this.headerFive1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderFive();
+            this.testing1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Control.Testing();
             this.headerOne1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderOne();
+            this.headerFive1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderFive();
+            this.headerFour1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderFour();
+            this.headerThree1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderThree();
+            this.headerTwo1 = new TheBetterLimited_ManagementSystem.Lib.Toolkit.Font.HeaderTwo();
             this.SuspendLayout();
             // 
-            // headerTwo1
+            // testing1
             // 
-            this.headerTwo1.AutoSize = true;
-            this.headerTwo1.Font = new System.Drawing.Font("Courier New", 24F);
-            this.headerTwo1.Location = new System.Drawing.Point(27, 79);
-            this.headerTwo1.Name = "headerTwo1";
-            this.headerTwo1.Size = new System.Drawing.Size(205, 36);
-            this.headerTwo1.TabIndex = 1;
-            this.headerTwo1.Text = "headerTwo1";
-            // 
-            // headerThree1
-            // 
-            this.headerThree1.AutoSize = true;
-            this.headerThree1.Font = new System.Drawing.Font("Courier New", 18F);
-            this.headerThree1.Location = new System.Drawing.Point(28, 136);
-            this.headerThree1.Name = "headerThree1";
-            this.headerThree1.Size = new System.Drawing.Size(180, 27);
-            this.headerThree1.TabIndex = 2;
-            this.headerThree1.Text = "headerThree1";
-            // 
-            // headerFour1
-            // 
-            this.headerFour1.AutoSize = true;
-            this.headerFour1.Font = new System.Drawing.Font("Courier New", 16F);
-            this.headerFour1.Location = new System.Drawing.Point(29, 180);
-            this.headerFour1.Name = "headerFour1";
-            this.headerFour1.Size = new System.Drawing.Size(153, 24);
-            this.headerFour1.TabIndex = 3;
-            this.headerFour1.Text = "headerFour1";
-            // 
-            // headerFive1
-            // 
-            this.headerFive1.AutoSize = true;
-            this.headerFive1.Font = new System.Drawing.Font("Courier New", 13F);
-            this.headerFive1.Location = new System.Drawing.Point(29, 225);
-            this.headerFive1.Name = "headerFive1";
-            this.headerFive1.Size = new System.Drawing.Size(130, 20);
-            this.headerFive1.TabIndex = 4;
-            this.headerFive1.Text = "headerFive1";
+            this.testing1.Location = new System.Drawing.Point(433, 92);
+            this.testing1.Name = "testing1";
+            this.testing1.Size = new System.Drawing.Size(199, 71);
+            this.testing1.TabIndex = 6;
+            this.testing1.Text = "dfsdf";
             // 
             // headerOne1
             // 
@@ -85,11 +54,52 @@
             this.headerOne1.TabIndex = 5;
             this.headerOne1.Text = "headerOne1";
             // 
+            // headerFive1
+            // 
+            this.headerFive1.AutoSize = true;
+            this.headerFive1.Font = new System.Drawing.Font("Courier New", 13F);
+            this.headerFive1.Location = new System.Drawing.Point(29, 225);
+            this.headerFive1.Name = "headerFive1";
+            this.headerFive1.Size = new System.Drawing.Size(130, 20);
+            this.headerFive1.TabIndex = 4;
+            this.headerFive1.Text = "headerFive1";
+            // 
+            // headerFour1
+            // 
+            this.headerFour1.AutoSize = true;
+            this.headerFour1.Font = new System.Drawing.Font("Courier New", 16F);
+            this.headerFour1.Location = new System.Drawing.Point(29, 180);
+            this.headerFour1.Name = "headerFour1";
+            this.headerFour1.Size = new System.Drawing.Size(153, 24);
+            this.headerFour1.TabIndex = 3;
+            this.headerFour1.Text = "headerFour1";
+            // 
+            // headerThree1
+            // 
+            this.headerThree1.AutoSize = true;
+            this.headerThree1.Font = new System.Drawing.Font("Courier New", 18F);
+            this.headerThree1.Location = new System.Drawing.Point(28, 136);
+            this.headerThree1.Name = "headerThree1";
+            this.headerThree1.Size = new System.Drawing.Size(180, 27);
+            this.headerThree1.TabIndex = 2;
+            this.headerThree1.Text = "headerThree1";
+            // 
+            // headerTwo1
+            // 
+            this.headerTwo1.AutoSize = true;
+            this.headerTwo1.Font = new System.Drawing.Font("Courier New", 24F);
+            this.headerTwo1.Location = new System.Drawing.Point(27, 79);
+            this.headerTwo1.Name = "headerTwo1";
+            this.headerTwo1.Size = new System.Drawing.Size(205, 36);
+            this.headerTwo1.TabIndex = 1;
+            this.headerTwo1.Text = "headerTwo1";
+            // 
             // TestText
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testing1);
             this.Controls.Add(this.headerOne1);
             this.Controls.Add(this.headerFive1);
             this.Controls.Add(this.headerFour1);
@@ -109,5 +119,6 @@
         private Lib.Toolkit.Font.HeaderFour headerFour1;
         private Lib.Toolkit.Font.HeaderFive headerFive1;
         private Lib.Toolkit.Font.HeaderOne headerOne1;
+        private Lib.Toolkit.Control.Testing testing1;
     }
 }
