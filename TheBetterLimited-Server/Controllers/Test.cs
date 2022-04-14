6d8db795227e0e52254458a86fefb1c5843364e1
Ryuuu825 <Ryuuu825@protonmail.com>
2022-04-14 18:47:41 +0800

    test the new api

---
+++ TheBetterLimited-Server/Controllers/Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace TheBetterLimited_Server.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+
+        [HttpGet("json")]
+        public IActionResult Get(JObject payload)
+        {
+            return Ok(payload.ToString());
+        }
+
+        
+    }
+}
