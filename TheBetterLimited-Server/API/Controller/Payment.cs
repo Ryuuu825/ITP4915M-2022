fc62cdc55c583329f3a5e5ee7de0f7a29ef9dba9
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-05-24 23:45:41 +0800

    for #17
    finish the structure of the payment gateway

--- TheBetterLimited-Server/API/Controller/Payment.cs
+++ TheBetterLimited-Server/API/Controller/Payment.cs
@@ -15,11 +15,24 @@ namespace TheBetterLimited_Server.API.Controller
             controller = new PaymentController(db);
         }
 
-        [HttpPost("create")]
+        [HttpPost("cash/create")]
         public async Task<IActionResult> CreatePayment( [FromBody] Data.Dto.TransactionDto req)
         {
             Data.Dto.TransactionResDto res = await controller.CreatePayment(req);
             return Ok(res);
+        }
+
+        [HttpPost("creditcard/create")]
+        public async Task<IActionResult> CreateCreditCard( [FromBody] Data.Dto.CreditCardTransactionDto req)
+        {
+            Data.Dto.TransactionResDto res = await controller.CreateCreditCardPayment(req);
+            return Ok(res);
+        }
+
+        [HttpPost("qrcode")]
+        public async Task<IActionResult> GetPaymentQRCode()
+        {
+            return File(await controller.GetPaymentQRCode() , "image/png");
         }
     }
 }
--- TheBetterLimited-Server/Data/Dto/TransactionDto.cs
+++ TheBetterLimited-Server/Data/Dto/TransactionDto.cs
@@ -6,22 +6,39 @@ namespace TheBetterLimited_Server.Data.Dto
         public decimal Amount { get; set; }
         public string Currency { get; set; }
     }
+
+    public class CreditCardTransactionDto : TransactionDto
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
     public class TransactionResDto {}
     public class TransactionOkDto : TransactionResDto
     {
         public string Id { get; set; }
-        public Guid ReferenceNumber { get; set; }
         public DateTime CreatedAt { get; set; }
 
         public decimal Amount { get; set; }
 
         public string Currency { get; set; }
 
+        public string PaymentMethod { get; set; }
+
     }
 
     public class TransactionErrorDto : TransactionResDto
     {
         public string ErrorMessage { get; set; }
-        public Guid ReferenceNumber { get; set; }
     }
+
+    public class CreditCardTransactionOkDto : TransactionOkDto
+    {
+        public string ReferenceNumber { get; set; }
+    }
+
+    public class CreditCardTransactionErrorDto : TransactionErrorDto
+    {
+        public string ReferenceNumber { get; set; }
+    }
+
 }
