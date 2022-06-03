3416e4afe08eb8db790aec4c1c849b5e0f345243
Ben POON <jyuban@outlook.com>
2022-06-04 02:11:45 +0800

    Add some POS sub pages

---
+++ TheBetterLimited-System/Views/POS/PendingOption.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TheBetterLimited.Views
+{
+    partial class PendingOption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainForm = new System.Windows.Forms.Panel();
+            this.BottomBtn = new System.Windows.Forms.Panel();
+            this.CancelBtn = new TheBetterLimited.CustomizeControl.RoundButton();
+            this.ConfirmBtn = new TheBetterLimited.CustomizeControl.RoundButton();
+            this.CustomerInfo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.UserInfoHeaderContainer = new System.Windows.Forms.Panel();
+            this.Title = new System.Windows.Forms.Label();
+            this.pendingOptionPicBox2 = new TheBetterLimited.CustomizeControl.PendingOptionPicBox();
+            this.pendingOptionPicBox1 = new TheBetterLimited.CustomizeControl.PendingOptionPicBox();
+            this.MainForm.SuspendLayout();
+            this.BottomBtn.SuspendLayout();
+            this.CustomerInfo.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.UserInfoHeaderContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.MainForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.MainForm.Controls.Add(this.BottomBtn);
+            this.MainForm.Controls.Add(this.CustomerInfo);
+            this.MainForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainForm.Location = new System.Drawing.Point(0, 0);
+            this.MainForm.Margin = new System.Windows.Forms.Padding(2);
+            this.MainForm.Name = "MainForm";
+            this.MainForm.Size = new System.Drawing.Size(497, 362);
+            this.MainForm.TabIndex = 3;
+            // 
+            // BottomBtn
+            // 
+            this.BottomBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.BottomBtn.Controls.Add(this.CancelBtn);
+            this.BottomBtn.Controls.Add(this.ConfirmBtn);
+            this.BottomBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BottomBtn.Location = new System.Drawing.Point(0, 296);
+            this.BottomBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.BottomBtn.Name = "BottomBtn";
+            this.BottomBtn.Padding = new System.Windows.Forms.Padding(10, 10, 10, 0);
+            this.BottomBtn.Size = new System.Drawing.Size(497, 66);
+            this.BottomBtn.TabIndex = 34;
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.BackColor = System.Drawing.Color.Silver;
+            this.CancelBtn.BackgroundColor = System.Drawing.Color.Silver;
+            this.CancelBtn.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.CancelBtn.BorderRadius = 10;
+            this.CancelBtn.BorderSize = 0;
+            this.CancelBtn.FlatAppearance.BorderSize = 0;
+            this.CancelBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(182)))), ((int)(((byte)(99)))));
+            this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.White;
+            this.CancelBtn.IsChecked = false;
+            this.CancelBtn.Location = new System.Drawing.Point(71, 13);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(145, 40);
+            this.CancelBtn.TabIndex = 1;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.TextColor = System.Drawing.Color.White;
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            // 
+            // ConfirmBtn
+            // 
+            this.ConfirmBtn.BackColor = System.Drawing.Color.SeaGreen;
+            this.ConfirmBtn.BackgroundColor = System.Drawing.Color.SeaGreen;
+            this.ConfirmBtn.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.ConfirmBtn.BorderRadius = 10;
+            this.ConfirmBtn.BorderSize = 0;
+            this.ConfirmBtn.FlatAppearance.BorderSize = 0;
+            this.ConfirmBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConfirmBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConfirmBtn.ForeColor = System.Drawing.Color.White;
+            this.ConfirmBtn.IsChecked = false;
+            this.ConfirmBtn.Location = new System.Drawing.Point(270, 13);
+            this.ConfirmBtn.Name = "ConfirmBtn";
+            this.ConfirmBtn.Size = new System.Drawing.Size(145, 40);
+            this.ConfirmBtn.TabIndex = 0;
+            this.ConfirmBtn.Text = "Comfirm";
+            this.ConfirmBtn.TextColor = System.Drawing.Color.White;
+            this.ConfirmBtn.UseVisualStyleBackColor = false;
+            // 
+            // CustomerInfo
+            // 
+            this.CustomerInfo.BackColor = System.Drawing.Color.Transparent;
+            this.CustomerInfo.Controls.Add(this.panel1);
+            this.CustomerInfo.Controls.Add(this.UserInfoHeaderContainer);
+            this.CustomerInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CustomerInfo.Location = new System.Drawing.Point(0, 0);
+            this.CustomerInfo.Name = "CustomerInfo";
+            this.CustomerInfo.Size = new System.Drawing.Size(497, 362);
+            this.CustomerInfo.TabIndex = 24;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.panel1.Controls.Add(this.pendingOptionPicBox2);
+            this.panel1.Controls.Add(this.pendingOptionPicBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 10);
+            this.panel1.Size = new System.Drawing.Size(497, 322);
+            this.panel1.TabIndex = 27;
+            // 
+            // UserInfoHeaderContainer
+            // 
+            this.UserInfoHeaderContainer.BackColor = System.Drawing.Color.White;
+            this.UserInfoHeaderContainer.Controls.Add(this.Title);
+            this.UserInfoHeaderContainer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.UserInfoHeaderContainer.Location = new System.Drawing.Point(0, 0);
+            this.UserInfoHeaderContainer.Name = "UserInfoHeaderContainer";
+            this.UserInfoHeaderContainer.Size = new System.Drawing.Size(497, 40);
+            this.UserInfoHeaderContainer.TabIndex = 20;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(3, 7);
+            this.Title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(226, 25);
+            this.Title.TabIndex = 6;
+            this.Title.Text = "Pending Pick Up Option";
+            // 
+            // pendingOptionPicBox2
+            // 
+            this.pendingOptionPicBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pendingOptionPicBox2.BorderColor = System.Drawing.Color.LightGray;
+            this.pendingOptionPicBox2.BorderRadius = 0;
+            this.pendingOptionPicBox2.BorderSelectedColor = System.Drawing.Color.Black;
+            this.pendingOptionPicBox2.BorderSize = 2;
+            this.pendingOptionPicBox2.Image = global::TheBetterLimited.Properties.Resources.delivery_truck;
+            this.pendingOptionPicBox2.IsSelected = false;
+            this.pendingOptionPicBox2.Location = new System.Drawing.Point(282, 32);
+            this.pendingOptionPicBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.pendingOptionPicBox2.Name = "pendingOptionPicBox2";
+            this.pendingOptionPicBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.pendingOptionPicBox2.Size = new System.Drawing.Size(169, 191);
+            this.pendingOptionPicBox2.TabIndex = 1;
+            this.pendingOptionPicBox2.Title = "Delivery";
+            // 
+            // pendingOptionPicBox1
+            // 
+            this.pendingOptionPicBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pendingOptionPicBox1.BorderColor = System.Drawing.Color.LightGray;
+            this.pendingOptionPicBox1.BorderRadius = 0;
+            this.pendingOptionPicBox1.BorderSelectedColor = System.Drawing.Color.Black;
+            this.pendingOptionPicBox1.BorderSize = 2;
+            this.pendingOptionPicBox1.Image = global::TheBetterLimited.Properties.Resources.booking;
+            this.pendingOptionPicBox1.IsSelected = false;
+            this.pendingOptionPicBox1.Location = new System.Drawing.Point(39, 32);
+            this.pendingOptionPicBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.pendingOptionPicBox1.Name = "pendingOptionPicBox1";
+            this.pendingOptionPicBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.pendingOptionPicBox1.Size = new System.Drawing.Size(166, 191);
+            this.pendingOptionPicBox1.TabIndex = 0;
+            this.pendingOptionPicBox1.Title = "Booking";
+            // 
+            // PendingOption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(497, 362);
+            this.Controls.Add(this.MainForm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1200, 900);
+            this.Name = "PendingOption";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pick Up Option";
+            this.MainForm.ResumeLayout(false);
+            this.BottomBtn.ResumeLayout(false);
+            this.CustomerInfo.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.UserInfoHeaderContainer.ResumeLayout(false);
+            this.UserInfoHeaderContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainForm;
+        private System.Windows.Forms.Panel CustomerInfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel UserInfoHeaderContainer;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Panel BottomBtn;
+        private CustomizeControl.RoundButton CancelBtn;
+        private CustomizeControl.RoundButton ConfirmBtn;
+        private CustomizeControl.PendingOptionPicBox pendingOptionPicBox2;
+        private CustomizeControl.PendingOptionPicBox pendingOptionPicBox1;
+    }
+}
---
+++ TheBetterLimited-System/Views/POS/WaitResult.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TheBetterLimited.Views
+{
+    partial class WaitResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CancelBtn = new TheBetterLimited.CustomizeControl.RoundButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.BackColor = System.Drawing.Color.Silver;
+            this.CancelBtn.BackgroundColor = System.Drawing.Color.Silver;
+            this.CancelBtn.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.CancelBtn.BorderRadius = 10;
+            this.CancelBtn.BorderSize = 0;
+            this.CancelBtn.FlatAppearance.BorderSize = 0;
+            this.CancelBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(182)))), ((int)(((byte)(99)))));
+            this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.White;
+            this.CancelBtn.IsChecked = false;
+            this.CancelBtn.Location = new System.Drawing.Point(125, 172);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(85, 40);
+            this.CancelBtn.TabIndex = 1;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.TextColor = System.Drawing.Color.White;
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.CancelBtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 224);
+            this.panel1.TabIndex = 36;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Waiting ...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TheBetterLimited.Properties.Resources.Loading_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(119, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // WaitResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(338, 224);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1200, 900);
+            this.Name = "WaitResult";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Waiting";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private CustomizeControl.RoundButton CancelBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
