9e1a7c62410fb6296f82793580d3f7ff98ded52f
Ryu <85796869+Ryuuu825@users.noreply.github.com>
2022-06-29 22:04:53 +0800

    fixed get by id with composite primary key

--- TheBetterLimited-Server/API/Controller/Restock.cs
+++ TheBetterLimited-Server/API/Controller/Restock.cs
@@ -56,7 +56,7 @@ namespace TheBetterLimited_Server.API.Controller
 
                 foreach( var item in dto.Items)
                 {
-                    Data.Entity.Supplier_Goods_Stock sgs = _SGSTable.GetById(item._supplierGoodsStockId);
+                    Data.Entity.Supplier_Goods_Stock sgs =_SGSTable.GetBySQL($"SELECT * FROM `Supplier_Goods_Stock` WHERE `Id` = '{item._supplierGoodsStockId}'").FirstOrDefault();
                     Data.Entity.RestockRequest_Supplier_Goods rgs = new Data.Entity.RestockRequest_Supplier_Goods
                     {
                         _restockRequestId = rr.ID,
